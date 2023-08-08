4f2adc59ae12b985544971bb298d0ae186567968
Josiah <63004866+Necl0@users.noreply.github.com>
2023-08-08 14:20:01 -0400

    Update NumberSpiral.py

--- CSES/NumberSpiral.py
+++ CSES/NumberSpiral.py
@@ -1,5 +1,13 @@
-t = int(input())
-
-for i in range(t):
+for i in range(int(input())):
     y, x = list(map(int, input().split()))
+    m = max(y, x)
 
+    d = sum([2 * i for i in range(1, m)] + [1])
+
+    if y==x:
+        print(d)
+        continue
+    elif m%2==1:
+        print(d+x-y)
+    else:
+        print(d+y-x)
